c338839c4e0e774aeac776a63185b03e1868137a
melchi komi <mkomi.redh@gmail.com>
2025-04-10 13:57:35 -0500

    MathApp

---
+++ MathApp/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+
+
+        double bobSalary = 50000;
+        double garySalary = 200000.99;
+
+        double highestSalary = garySalary;
+
+        System.out.println("Highest Salary is: " + highestSalary);
+//question 2
+      double carPrice = 34000;
+
+      double truckPrice = 15000;
+
+      double lowestPrice = Math.min(carPrice, truckPrice);
+
+      System.out.println("lowest Price is: " + lowestPrice);
+
+      //question 3
+
+        double radius = 7.25;
+
+        double area = Math.PI * Math.pow(radius, 2);
+
+        System.out.println(area);
+
+        double x = 5.0;
+
+        double sqrtX = Math.sqrt(x);
+
+        System.out.println("The square root of x is: " + sqrtX);
+
+        // q.5
+
+        int x1 = 5,  y1 =10;
+        int x2 = 85, y2 = 50;
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The distance between the points are: " + distance);
+
+
+
+            double value1 = -3.8;
+
+            double absolutegit =
+
+    }
+
+}
+
+
